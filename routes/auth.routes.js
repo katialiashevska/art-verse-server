dbfba47ec2dcc1bc4b879a837608ee2465a90249
Katia <kate.liashevska@gmail.com>
2023-08-25 17:41:08 +0200

    minor error message correction

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -111,10 +111,10 @@ router.post("/login", (req, res, next) => {
                 // Send the token as the response
                 res.status(200).json({ authToken: authToken })
             } else {
-                res.status(401).json({ message: "Unable to authenticate the user" })
+                res.status(401).json({ message: "Unable to authenticate the user." })
             }
         })
-        .catch(err => res.status(500).json({ message: "Internal Server Error" }))
+        .catch(err => res.status(500).json({ message: "Internal Server Error." }))
 })
 
 // GET  /auth/verify  -  Used to verify JWT stored on the client
